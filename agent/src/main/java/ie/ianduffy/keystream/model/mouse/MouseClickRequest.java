ff5a7fd2ee29ed005c56a66a2354bb45bff6efdc
Ian Duffy <ian@ianduffy.ie>
2015-06-01 16:00:43 +0100

    remove print out statements

--- agent/src/main/java/ie/ianduffy/keystream/model/mouse/MouseClickRequest.java
+++ agent/src/main/java/ie/ianduffy/keystream/model/mouse/MouseClickRequest.java
@@ -10,8 +10,6 @@ public class MouseClickRequest implements MouseRequest {
     private MouseButtons button;
 
     public void visit(Robot robot) {
-        System.out.println(button.getCode());
-        System.out.println(InputEvent.BUTTON1_MASK);
         robot.mousePress(button.getCode());
         robot.mouseRelease(button.getCode());
     }
